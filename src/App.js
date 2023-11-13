fed0154fd659991ce81f7f4ff68f9a5db2dee0e8
thanhphong <nguyenthanhphong03.tpwin@gmail.com>
2023-11-13 12:36:35 +0700

    add code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Menu from "./components/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
